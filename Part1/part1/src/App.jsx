db758e182cf74d1a0fcf0e5c82297728e8547e74
Lucas Piorno <lucasp01012002@gmail.com>
2023-12-02 11:25:13 +0200

    ej 1_1 done

---
+++ Part1/part1/src/App.jsx
@@ -0,0 +1,39 @@
+const Header = (name) => {
+  return (
+    <div>{name}</div>
+  )
+}
+
+const Content = (part, exercises) => {
+  return (
+    <p>{part} {exercises}</p>
+  )
+}
+
+const Total = (n_exercises) => {
+  return (
+    <p>Number of exercises {n_exercises}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <h1>{Header(course)}</h1>
+        {Content(part1, exercises1)}
+        {Content(part2, exercises2)}
+        {Content(part3, exercises3)}
+      {Total(exercises1 + exercises2 + exercises3)}
+    </div>
+  )
+}
+
+export default App
