6ba3931398c44e60855442379424570649a31290
Lucas Piorno <lucasp01012002@gmail.com>
2023-12-02 12:22:42 +0200

    ex 1_4

--- Part1/part1/src/App.jsx
+++ Part1/part1/src/App.jsx
@@ -28,24 +28,25 @@ const Total = (n_exercises) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   return (
     <div>
       <h1>{Header(course)}</h1>
-      {Content( [part1, part2, part3] )}
-      {Part({name: "Total", exercises: part1.exercises + part2.exercises + part3.exercises})}
+      {Content( parts )}
+      {Part({name: "Total", exercises: parts[0].exercises + parts[1].exercises + parts[2].exercises})}
     </div>
   )
 }
