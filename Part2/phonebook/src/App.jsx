151b1a6328b5c1018cf425b12b8920018a55f26e
Lucas Piorno <lucasp01012002@gmail.com>
2023-12-03 22:56:07 +0200

    phonebook step 2_11

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 const Input = ({ namee, onChange }) => {
   return (
@@ -44,18 +46,23 @@ const App = () => {
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
 
-  const [shownContacts, setShownContacts] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
-  console.log('Shwon contacts', shownContacts)
+  // Fetch contacts from server
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/contacts')
+      .then(response => {
+        console.log('response', response)
+        setContacts(response.data)
+        setShownContacts(response.data)
+        }
+      )
+    },
+    [])
   
+  // New contact form logic
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-
+  
   const onSubmitNewContact = (event) => {
     event.preventDefault()
     const contactObject = {
@@ -67,39 +74,47 @@ const App = () => {
     setNewName('')
     setNewNumber('')
   }
-
+  
   const handlePersonChange = (event) => {
     setNewName(event.target.value)
   }
-
+  
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+  
+  // Search filter logic
+  const [shownContacts, setShownContacts] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
 
   const handleFilterChange = (event) => {
     const filter = event.target.value
     console.log('filter', filter)
     setShownContacts(contacts.filter(
       contact => contact.name.toLowerCase().includes(filter.toLowerCase())
-      ))
+    ))
   }
-
+    
   return (
     <div>
-      <h2>Phonebook</h2>
-      <ContactList contactList={shownContacts} />
+    <h2>Phonebook</h2>
+    <ContactList contactList={shownContacts} />
 
-      <h2>Filter contacts</h2>
-      <input onChange={handleFilterChange} />
+    <h2>Filter contacts</h2>
+    <input onChange={handleFilterChange} />
 
-      <h2>Add a new contact</h2>
+    <h2>Add a new contact</h2>
 
-      {Form({ onSubmit: onSubmitNewContact, 
-        children: [
-          { name: 'name', onChange: handlePersonChange }, 
-          { name: 'number', onChange: handleNumberChange }] 
-      })}
-    </div>
+    {Form({ onSubmit: onSubmitNewContact, 
+      children: [
+        { name: 'name', onChange: handlePersonChange }, 
+        { name: 'number', onChange: handleNumberChange }] 
+    })}
+  </div>
   )
 }
 
