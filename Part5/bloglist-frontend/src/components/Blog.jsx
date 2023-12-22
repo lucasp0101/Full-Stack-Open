4a0de5d8ce7715189d6bf73a5e0cab230287d97f
Lucas Piorno <lucasp01012002@gmail.com>
2023-12-22 14:09:49 +0200

    5.7 added with style(s)

--- Part5/bloglist-frontend/src/components/Blog.jsx
+++ Part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const styleSection = {
+    'border': '2px solid',
+    'display': 'flex',
+    'justify-content': 'center',
+    'width': 'fit-content',
+    'margin': '12px'
+  }
+  const styleContent = {
+    'flex': 'none',
+  }
+  const [detail, setDetail] = useState(false)
+
+  const toggleDetail = () => {
+    setDetail(!detail)
+  }
+
+  if (detail){
+    return (
+      <div style={styleSection}>
+        <div style={styleContent}>
+          {blog.title} <button onClick={toggleDetail}>Hide</button><br/>
+          {blog.author} <br/>
+          {blog.likes} <button>Like</button><br/>
+          {blog.url} <br/>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div style={styleSection}>
+      {blog.title} <button onClick={toggleDetail}>Show</button><br/>
+    </div>
+  )
+}
 
 export default Blog
